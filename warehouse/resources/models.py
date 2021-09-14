822483ffba6684fcfd8b2ac78f82df9315ed738d
Ilya Sukhoplyuev <suhoy@gramend.ru>
2021-09-14 20:25:04 +0400

    Модель Товаров на складе (WarehouseRecourse)

--- warehouse/resources/models.py
+++ warehouse/resources/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+
+class WarehouseResource(models.Model):
+    MEASURE_UNITS = [
+        ('шт', 'штук'),
+        ('кг', 'киллограм'),
+        ('л', 'литров'),
+    ]
+
+    name = models.CharField("Название товара", max_length=255)
+    quantity = models.PositiveIntegerField("Количество")
+    measure_unit = models.CharField("Единица измерения", max_length=10, choices=MEASURE_UNITS)
+    cost = models.PositiveIntegerField("Цена")
+    last_update = models.DateTimeField("Дата последней поставки/отгрузки", auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.quantity} {self.measure_unit} X {self.cost})"
+
+    @property
+    def total_cost(self):
+        return self.cost * self.quantity
+
--- warehouse/warehouse/settings.py
+++ warehouse/warehouse/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'resources.apps.ResourcesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
